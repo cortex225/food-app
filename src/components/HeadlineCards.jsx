4feeca0ec6686d3367117b5a16a90a2ebd21b5ec
cortex225 <48181439+cortex225@users.noreply.github.com>
2023-06-23 10:43:20 -0400

    init

---
+++ src/components/HeadlineCards.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+const HeadlineCards = () => {
+  return (
+    <div className="m-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6 ">
+      {/* Card */}
+      <div className="rounded-xl relative">
+        {/* Overlay */}
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">Sun's Out, BOGO's Out</p>
+          <p className="px-2">Through 8/26</p>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+            Order Now
+          </button>
+        </div>
+        <img
+          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://images.pexels.com/photos/2293537/pexels-photo-2293537.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt="card"
+        />
+      </div>
+      <div className="rounded-xl relative">
+        {/* Overlay */}
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">Sun's Out, BOGO's Out</p>
+          <p className="px-2">Through 8/26</p>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+            Order Now
+          </button>
+        </div>
+        <img
+          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://images.pexels.com/photos/2293537/pexels-photo-2293537.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt="card"
+        />
+      </div>
+      <div className="rounded-xl relative">
+        {/* Overlay */}
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">Sun's Out, BOGO's Out</p>
+          <p className="px-2">Through 8/26</p>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+            Order Now
+          </button>
+        </div>
+        <img
+          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://images.pexels.com/photos/2293537/pexels-photo-2293537.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt="card"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default HeadlineCards;
