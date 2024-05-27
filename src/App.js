a3eed98686abba049dd290cc4659bf288dd1856e
jlgouaho <1900751@CegepGranby.qc.ca>
2024-05-27 12:53:00 -0400

    mise à jour

--- src/App.js
+++ src/App.js
@@ -4,15 +4,12 @@ import Hero from "./components/Hero";
 import HeadlineCards from "./components/HeadlineCards";
 import Food from "./components/Food";
 import Category from "./components/Category";
-import PreviewCard from "./components/PreviewCard";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
       <HeadlineCards />
-      <PreviewCard imageUrl="/img/car-rental-full.c58b37da333d73238fdd.webp" />
-
       <Food />
       <Category />
     </div>
